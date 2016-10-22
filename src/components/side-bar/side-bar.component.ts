afc18d85d5108b3b4a0a98174b5262c1de55bdbf
Fredrik Lund Henriksen <fredrik.henriksen@vikenfiber.no>
2016-10-22 15:59:56 +0200

    Kinda working expandible list

--- src/components/side-bar/side-bar.component.ts
+++ src/components/side-bar/side-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit }    from "@angular/core";
+import { Component, OnInit, AfterViewInit, OnChanges }    from "@angular/core";
 
 import { OrgUnit }  from "../../core/org-unit";
 
@@ -6,13 +6,15 @@ import { OrgUnitService } from "../../services/org-unit.service";
 
 import { OrgUnitUpdate} from "../../core/org-unit-update.interface";
 
+declare var $:any;
+
 @Component({
     selector: "side-bar",
     template: require<any>("./side-bar.component.html"),
     styles: [ require<any>("./side-bar.component.less") ]
 })
 
-export class SideBarComponent implements OrgUnitUpdate {
+export class SideBarComponent implements OrgUnitUpdate, AfterViewInit, OnChanges {
     private orgUnits: OrgUnit[] = null;
 
     constructor(private orgUnitService: OrgUnitService) {}
@@ -24,6 +26,40 @@ export class SideBarComponent implements OrgUnitUpdate {
 
     ngOnInit(): void {
         this.orgUnitService.registerOrgUnitUpdateListener(this);
+    }
+
+    ngAfterViewInit() {
+ 
+    }
+
+    ngOnChanges(): void {
+        console.log("CHAAANGE");
+    }
+
+    test(): void {
+        console.log("teeest");
+        $.fn.togglepanels = function(){
+            return this.each(function(){
+                $(this).addClass("ui-accordion ui-accordion-icons ui-widget ui-helper-reset")
+            .find("h3")
+                .addClass("ui-accordion-header ui-helper-reset ui-state-default ui-corner-top ui-corner-bottom")
+                .hover(function() { $(this).toggleClass("ui-state-hover"); })
+                .prepend('<span class="ui-icon ui-icon-triangle-1-e"></span>')
+                .click(function() {
+                $(this)
+                    .toggleClass("ui-accordion-header-active ui-state-active ui-state-default ui-corner-bottom")
+                    .find("> .ui-icon").toggleClass("ui-icon-triangle-1-e ui-icon-triangle-1-s").end()
+                    .next().slideToggle();
+                return false;
+                })
+                .next()
+                .addClass("ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom")
+                .hide();
+            });
+        };
+
+        $("#notaccordion").togglepanels();
+
     }
 
 }
