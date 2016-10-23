caabd75bf7724f71d26b858bcb442247597b7fcf
Fredrik Lund Henriksen <fredrik.henriksen@vikenfiber.no>
2016-10-23 15:28:28 +0200

    Toggle-able side bar, auto hides and shows

--- src/components/side-bar/side-bar.component.ts
+++ src/components/side-bar/side-bar.component.ts
@@ -14,16 +14,23 @@ declare var $: any;
     styles: [ require<any>("./side-bar.component.less") ]
 })
 
-export class SideBarComponent implements OrgUnitUpdate, AfterViewInit, OnChanges {
+export class SideBarComponent implements OrgUnitUpdate {
     private orgUnits: OrgUnit[] = null;
     private collapsList1: string[] = [];
     private collapsList2: string[] = [];
+
+    private sideBarVisible = true;
 
     constructor(private orgUnitService: OrgUnitService) {}
 
     onOrgUnitGet(orgUnits: OrgUnit[]): void {
         this.orgUnits = orgUnits;
         console.log("Side bar - received new orgUnits: " + this.orgUnits);
+        if (this.orgUnits === undefined || this.orgUnits === null || this.orgUnits.length === 0) {
+            this.toggleSideBar(false);
+        } else {
+            this.toggleSideBar(true);
+        }
     }
 
     ngOnInit(): void {
@@ -35,38 +42,30 @@ export class SideBarComponent implements OrgUnitUpdate, AfterViewInit, OnChanges
         }
     }
 
-    ngAfterViewInit() {
-        console.log("AFTER VIEW INIT");
-    }
+    toggleSideBar(show?: Boolean): void {
+        console.log("\nBefore:");
+        console.log("show = " + show);
+        console.log("sideBarVisible = " + this.sideBarVisible);
+        
+        
+        if (show === undefined) {
+            $("#sideBar").toggle("show");
+            this.sideBarVisible = !this.sideBarVisible;
+        }
 
-    ngOnChanges(): void {
-        console.log("CHAAANGE");
-    }
-
-    test(): void {
-        console.log("teeest");
-        $.fn.togglepanels = function(){
-            return this.each(function(){
-                $(this).addClass("ui-accordion ui-accordion-icons ui-widget ui-helper-reset")
-            .find("h3")
-                .addClass("ui-accordion-header ui-helper-reset ui-state-default ui-corner-top ui-corner-bottom")
-                .hover(function() { $(this).toggleClass("ui-state-hover"); })
-                .prepend(`<span class="ui-icon ui-icon-triangle-1-e"></span>`)
-                .click(function() {
-                $(this)
-                    .toggleClass("ui-accordion-header-active ui-state-active ui-state-default ui-corner-bottom")
-                    .find("> .ui-icon").toggleClass("ui-icon-triangle-1-e ui-icon-triangle-1-s").end()
-                    .next().slideToggle();
-                return false;
-                })
-                .next()
-                .addClass("ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom")
-                .hide();
-            });
-        };
-
-        $("#notaccordion").togglepanels();
-
+        if (show === true && this.sideBarVisible !== true) {
+            $("#sideBar").toggle("show");
+            this.sideBarVisible = !this.sideBarVisible;
+        
+        } else if (show === false && this.sideBarVisible !== false) {
+            $("#sideBar").toggle("show");
+            this.sideBarVisible = !this.sideBarVisible;
+        }
+        
+        console.log("\nAfter:");
+        console.log("show = " + show);
+        console.log("sideBarVisible = " + this.sideBarVisible);
+        
     }
 
 }
