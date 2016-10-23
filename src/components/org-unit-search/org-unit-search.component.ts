d69d008b9af99ee39336fb665723587469f478b4
Fredrik Lund Henriksen <fredrik.henriksen@vikenfiber.no>
2016-10-23 14:54:21 +0200

    Removed uneeded code

--- src/components/org-unit-search/org-unit-search.component.ts
+++ src/components/org-unit-search/org-unit-search.component.ts
@@ -29,31 +29,6 @@ export class OrgUnitSearchComponent implements OnInit {
     }
 
     advancedSearch(): void {
-        console.log("yo");
-
-
-        $.fn.togglepanels = function(){
-            return this.each(function(){
-                $(this)
-                .find("h5")
-                .addClass("ui-accordion-header ui-helper-reset ui-state-default ui-corner-top ui-corner-bottom")
-                .hover(function() { $(this).toggleClass("ui-state-hover"); })
-                .prepend(`<span class="ui-icon ui-icon-triangle-1-e"></span>`)
-                .click(function() {
-                $(this)
-                    .toggleClass("ui-accordion-header-active ui-state-active ui-state-default ui-corner-bottom")
-                    .find("> .ui-icon").toggleClass("ui-icon-triangle-1-e ui-icon-triangle-1-s").end()
-                    .next().slideToggle();
-                return false;
-                })
-                .next()
-                .addClass("ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom")
-                .hide();
-            });
-        };
-
-        $("#notaccordion").togglepanels();
-
         $("#advancedSearchDiv").toggle("show");
     }
 
